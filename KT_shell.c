2fa2d792756c403997e513debf6f7c1371effb4e
kutloanozwane <kutloanozwane2012@gmail.com>
2023-12-13 12:23:33 -0800

    Simple Shell

--- KT_shell.c
+++ KT_shell.c
@@ -1,10 +1,13 @@
 #include "shell.h"
+#include <sys/wait.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #define MAX_COMMAND_LENGTH 100
+#define MAX_ARGUMENTS 20
 
 /**
  * prompt - should display the shell prompt and
@@ -13,55 +16,59 @@
  *
  * Return: Always
  */
-void handleExit(void)
+void prompt(char *nameofShell)
 {
-	printf("Exiting the shell...\n");
-	exit(EXIT_SUCCESS);
-}
+char command[MAX_COMMAND_LENGTH];
+char *argv[MAX_ARGUMENTS];
+char *path = getenv("PATH");
+int argc;
+pid_t child_id;
+int status;
 
-void handleEnv(void)
+while (1)
 {
-	extern char **environ;
-	printf("Environment variables:\n");
-	for (int k = 0; environ[k] != NULL; k++)
-	{
-		printf("%s\n", environ[k]);
-	}
+if (isatty(STDIN_FILENO))
+{
+printf("%s## ", nameofShell);
+fflush(stdout);
 }
-
-void executeCommand(char *command) {
-    int status = system(command);
-
-    if (status == -1) {
-        perror("Command execution failed");
-    }
+if (fgets(command, sizeof(command), stdin) == NULL)
+{
+break;
 }
-
-void prompt(char *nameofShell) {
-    char command[MAX_COMMAND_LENGTH];
-
-    while (1) {
-        if (isatty(STDIN_FILENO)) {
-            printf("%s## ", nameofShell);
-            fflush(stdout);
-        }
-
-        if (fgets(command, sizeof(command), stdin) == NULL) {
-            break;
-        }
-
-        command[strcspn(command, "\n")] = '\0';
-
-        if (strcmp(command, "exit") == 0) {
-            handleExit();
-            break;
-        }
-
-        if (strcmp(command, "env") == 0) {
-            handleEnv();
-            continue;
-        }
-
-        executeCommand(command);
-    }
+/* Remove the trailing newline from the command */
+command[strcspn(command, "\n")] = '\0';
+/*  Exit Handling */
+if (strcmp(command, "exit") == 0)
+{
+break;
+}
+if (strcmp(command, "env") == 0)
+{
+printEnv();
+continue;
+}
+/* Tokenize the command into arguments */
+tokenizeCmd(command, argv, &argc);
+/* Execute the command */
+child_id = fork();
+if (child_id == -1)
+{
+perror("fork");
+continue;
+}
+else if (child_id == 0)
+{
+execCmd(argv[0], argv, path);
+exit(EXIT_FAILURE);
+}
+else
+{
+waitpid(child_id, &status, 0);
+if (WIFEXITED(status) && WEXITSTATUS(status) == 0)
+{
+continue;
+}
+}
+}
 }
